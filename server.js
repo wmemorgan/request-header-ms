9d0e174358d67405b983a4cf2df170babd1caa09
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-17 15:17:31 +0800

    Add feature to retrieve client operating system

--- server.js
+++ server.js
@@ -6,18 +6,23 @@ const express = require('express'),
 //Setting to obtain remote IP address when server is behind a proxy  
 app.set('trust proxy', true);
 
-const displayInfo = (ip, language) => {
+const displayInfo = (ip, language, os) => {
   return {
     ipaddress: ip,
     language: language,
-    // software: os 
+    software: os 
   }
 }
 
 app.get('/', (req, res) => {
-  let language = req.headers['accept-language']
-  language = language.substring(0, language.indexOf(',')); 
-  res.send(displayInfo(req.ip, language));
+  let language = req.headers['accept-language'],
+    os = req.headers['user-agent']
+  //Tidy-up header information
+  language = language.substring(0, language.indexOf(','));
+  os = os.substring(os.indexOf('(')+1, os.indexOf(')'));
+   
+  res.send(displayInfo(req.ip, language, os));
+
 })
 
 app.listen(port, () => {
