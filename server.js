0a97d374b81d9dd09a8a5e81e60871d10d76d63a
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-17 13:54:49 +0800

    Attempt to retrieve IP address

--- server.js
+++ server.js
@@ -3,10 +3,19 @@ const express = require('express'),
   path = require('path'),
   port = process.env.PORT || 3000;
 
-app.use('/public', express.static(process.cwd() + '/public'));
+// app.use('/public', express.static(process.cwd() + '/public'));
 
-app.route('/', (req, res) => {
-  res.sendFile(path.join(process.cwd() + '/views/index.html'));
+// app.route('/', (req, res) => {
+//   res.sendFile(path.join(process.cwd() + '/views/index.html'));
+// })
+
+app.get('/', (req, res) => {
+  var ip = req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    (req.socket.remoteAddress ? req.connection.socket.remoteAddress : null);
+
+  res.send(ip);
 })
 
 app.listen(port, () => {
