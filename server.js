22870e4616e2a0ba9dc401802a38e0b569300ff7
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-17 14:56:48 +0800

    Retrieve client language

--- server.js
+++ server.js
@@ -6,8 +6,18 @@ const express = require('express'),
 //Setting to obtain remote IP address when server is behind a proxy  
 app.set('trust proxy', true);
 
+const displayInfo = (ip, language) => {
+  return {
+    ipaddress: ip,
+    language: language,
+    // software: os 
+  }
+}
+
 app.get('/', (req, res) => {
-  res.send(req.ip);
+  let language = req.headers['accept-language']
+  language = language.substring(0, language.indexOf(',')); 
+  res.send(displayInfo(req.ip, language));
 })
 
 app.listen(port, () => {
